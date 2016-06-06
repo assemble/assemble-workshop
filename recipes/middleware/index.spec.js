88b08744f47e7f246625bbba06651074bf52e586
Stefan Walther <Stefan.Walther@qlik.com>
2016-06-06 02:28:36 +0200

    recipes(middleware): Sinon stubs.

--- recipes/middleware/index.spec.js
+++ recipes/middleware/index.spec.js
@@ -2,15 +2,36 @@
 'use strict';
 var chai = require( 'chai' );
 var expect = chai.expect;
-var chaiFs = require( 'chai-fs' );
+var sinon = require( 'sinon' );
 
-chai.use( chaiFs );
 var app = require( './index' );
 
-describe( 'Middleware', function () {
-	it( 'renders', function ( done ) {
+describe( 'middleware', function () {
+	it( 'events are emitted', function ( done ) {
+
+		var spyPreRender = sinon.spy();
+		app.on( 'preRender', spyPreRender );
+
+		var spyPreCompile = sinon.spy();
+		app.on( 'preCompile', spyPreCompile );
+
+		var spyPreLayout = sinon.spy();
+		app.on( 'preLayout', spyPreLayout );
+
+		var spyPostCompile = sinon.spy();
+		app.on( 'postCompile', spyPostCompile );
+
+		var spyPostRender = sinon.spy();
+		app.on( 'postRender', spyPostRender );
+
 		app.build( 'default', function ( err ) {
 			expect( err ).to.not.exist;
+
+			sinon.assert.calledTwice( spyPreRender );
+			sinon.assert.calledTwice( spyPreCompile );
+			sinon.assert.calledTwice( spyPreLayout );
+			sinon.assert.calledTwice( spyPostCompile );
+			sinon.assert.calledTwice( spyPostRender );
 			done();
 		} );
 	} );
