8cacf2f8b8750c9721cf4219d0f9d696b4e28cbe
Stefan Walther <Stefan.Walther@qlik.com>
2016-06-07 12:39:26 +0200

    Delete unnecessary config file

--- recipes/issue-permalinks-folders/config.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-var path = require( 'path' );
-
-module.exports = {
-	buildDir: path.join( __dirname, './.build' ),
-	srcDir: path.join( __dirname, './content/articles/**/*.{md,hbs}' )
-}
